ae41c5a14d01b2ae9ffea4190b72256d8dc92488
leekeunhwan <dlrmsghks7@gmail.com>
2018-04-02 19:39:56 +0900

    FDS 수업-HTML,CSS

--- ResponsiveWeb/js/webcafe.js
+++ ResponsiveWeb/js/webcafe.js
@@ -1,6 +1,23 @@
-var container = document.querySelector(".container");
-var grid = document.querySelector(".btn-grid");
+// var container = document.querySelector(".container");
+// var grid = document.querySelector(".btn-grid");
 
-grid.addEventListener("click", function() {
-  container.classList.toggle("is-act");
+// grid.addEventListener("click", function() {
+//   container.classList.toggle("is-act");
+// });
+
+// $(document).ready(function() {
+//   var video = $(".news-video");
+//   video.on("mouseover focusin", function() {
+//     $(this).attr("autoplay", "");
+//   });
+//   video.on("mouseout focusout", function() {
+//     $(this).attr("autoplay", "false");
+//   });
+// });
+
+var video1 = document.querySelector("video");
+video1.addEventListener("click", function() {
+  video1.autoplay = "true";
+  video1.load();
 });
+
