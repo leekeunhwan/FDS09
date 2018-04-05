eeffb180023aaafc0ff23ce198c4c6de3ee0dc98
leekeunhwan <dlrmsghks7@gmail.com>
2018-04-05 17:59:40 +0900

    FDS 수업-HTML,CSS

--- ResponsiveWeb/js/webcafe.js
+++ ResponsiveWeb/js/webcafe.js
@@ -1,23 +1,56 @@
-$(document).ready(function () {
+$(document).ready(function() {
+  var viewport = window.matchMedia("(max-width:999px)");
+  if (viewport.matches) {
+    var nav = $(".navigation");
+    var btn = $(".btn-menubar");
+    var menu = $(".menu");
+    var menuLists = $(".menu-list");
+    var menuItems = $(".menu-item");
+    var subMenus = $(".sub-menu");
 
-  var container = $('.container');
-  var grid = $('.btn-grid');
-  var video = $('.news-video');
-  video.get(0).volume = 0.0;
+    // 999px 이하에서 적용할 코드
+    menuItems.attr("aria-haspopup", "true");
+    menuItems.attr("aria-expanded", "false");
+    menuItems.attr("role", "button");
+    menuItems.attr("tabindex", "0");
+    menuItems.addClass("icon-plus");
 
-  video.on('mouseover focusin', function () {
-    this.play();
-    this.volume = 1.0;
-  });
+    btn.on("click", function(e) {
+      e.preventDefault();
+      nav.toggleClass("is-act");
+      if (nav.hasClass("is-act")) {
+        btn.attr("aria-label", "메인 메뉴 닫기");
+      } else {
+        btn.attr("aria-label", "메인 메뉴 열기");
+      }
+    });
 
-
-  video.on('mouseout focusout', function () {
-    this.pause();
-  });
-
-
-  grid.click(function () {
-    container.toggleClass('is-act');
-  });
-
+    menuItems.on("click keydown", function(e) {
+      if (e.type === "click" || (e.type === "keydown" && e.keyCode === 13)) {
+        menuLists.removeClass("is-act");
+        menuItems.addClass("icon-plus");
+        subMenus.css("display", "none");
+        $(this)
+          .parent()
+          .addClass("menu-act");
+        if (
+          $(this)
+            .parent()
+            .hasClass("menu-act")
+        ) {
+          $(this).attr("aria-expanded", "true");
+          $(this).addClass("icon-minus");
+          $(this).removeClass("icon-plus");
+          $(this)
+            .siblings()
+            .css("display", "block");
+          $(this).removeClass("menu-act");
+        } else {
+          $(this).attr("aria-expanded", "false");
+        }
+      }
+    });
+  } else {
+    // 1000px 이상에서 사용할 코드
+  }
 });
